2434d3710a089b3b463a6864feb231dd21c0b8e8
Fuad Hasanli <fhesenli92@gmail.com>
2025-06-29 13:07:08 +0400

    Update PhotoGalleryCrudController.php

--- app/Http/Controllers/Admin/PhotoGalleryCrudController.php
+++ app/Http/Controllers/Admin/PhotoGalleryCrudController.php
@@ -14,11 +14,11 @@ use Backpack\Pro\Http\Controllers\Operations\DropzoneOperation;
  */
 class PhotoGalleryCrudController extends CrudController
 {
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+//    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+//    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+//    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+//    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
 
     use DropzoneOperation;
 
